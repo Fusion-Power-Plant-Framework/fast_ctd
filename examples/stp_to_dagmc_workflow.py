17b7141fa5c078490cc48795c1e120d81c5ce357
Oliver Funk <oli.funk@gmail.com>
2025-04-28 16:30:41 +0100

    uncomments code in ex

--- examples/stp_to_dagmc_workflow.py
+++ examples/stp_to_dagmc_workflow.py
@@ -18,14 +18,14 @@ output_dagmc_file = "data/EUDEMO.h5m"
 merge_brep_geometries(brep_file, merged_brep_file)
 
 # # Facet BREP to DAGMC
-# facet_brep_to_dagmc(
-#     merged_brep_file,
-#     materials_def="data/EUDEMO-materials.csv",
-#     output_h5m_file=output_dagmc_file,
-#     tolerance=0.001,
-#     scale_factor=0.1,
-#     tol_is_absolute=False,
-# )
+facet_brep_to_dagmc(
+    merged_brep_file,
+    materials_def="data/EUDEMO-materials.csv",
+    output_h5m_file=output_dagmc_file,
+    tolerance=0.001,
+    scale_factor=0.1,
+    tol_is_absolute=False,
+)
 
 # then run (in terminal):
 # make_watertight data/EUDEMO.h5m -o data/EUDEMO-watertight.h5m
